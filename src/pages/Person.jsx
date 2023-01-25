a31597b85990436490026f068518a403ac0c14ed
mojtaba <mojtaba75rs@gmail.com>
2023-01-25 19:31:32 +0330

    Add spinner to Person Component when receiving information

--- src/pages/Person.jsx
+++ src/pages/Person.jsx
@@ -8,14 +8,13 @@ import PersonalInfo from "../components/pages/people/PersonalInfo";
 import Biography from "../components/pages/people/Biography";
 import KnownFor from "../components/pages/people/KnownFor";
 import Acting from "../components/pages/people/Acting";
+import { Loader } from "../components/constant";
 
 const Person = () => {
   const { personId } = useParams();
 
   const [person, setPerson] = useState([]);
   const [loading, setLoading] = useState(false);
-
-  console.log(person);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -36,26 +35,32 @@ const Person = () => {
     fetchData();
   }, []);
   return (
-    <Grid container sx={{ my: 5 }}>
-      <Grid xs={12} sm={7.2} md={4.5} lg={3.28} xl={2.5}>
-        <Box>
-          <Avatar
-            variant="rounded"
-            sx={{ maxWidth: 300,width:1, height: 450 }}
-            src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${person.profile_path}`}
-          />
-          <SocialLinks {...person.external_ids} />
-          <PersonalInfo {...person} />
-        </Box>
-      </Grid>
-      <Grid xs={12} sm={4.8} md={7.5} lg={8} xl={9.5}>
-        <Box sx={{ pl: { xs: 0, sm: 4 } }}>
-          <Biography {...person} />
-          <KnownFor {...person} />
-          <Acting {...person} />
-        </Box>
-      </Grid>
-    </Grid>
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <Grid container sx={{ my: 5 }}>
+          <Grid xs={12} sm={7.2} md={4.5} lg={3.28} xl={2.5}>
+            <Box>
+              <Avatar
+                variant="rounded"
+                sx={{ maxWidth: 300, width: 1, height: 450 }}
+                src={`https://image.tmdb.org/t/p/w300_and_h450_bestv2${person.profile_path}`}
+              />
+              <SocialLinks {...person.external_ids} />
+              <PersonalInfo {...person} />
+            </Box>
+          </Grid>
+          <Grid xs={12} sm={4.8} md={7.5} lg={8} xl={9.5}>
+            <Box sx={{ pl: { xs: 0, sm: 4 } }}>
+              <Biography {...person} />
+              <KnownFor {...person} />
+              <Acting {...person} />
+            </Box>
+          </Grid>
+        </Grid>
+      )}
+    </>
   );
 };
 export default Person;
