2ec762b984004c2781f5dfdffa40b8d87579ee0c
mojtaba <mojtaba75rs@gmail.com>
2023-03-07 21:07:01 +0330

    Refactoring the TvShowFacts component related to the tvShow page

--- src/components/pages/tvShows/Keywords.jsx
+++ src/components/pages/tvShows/Keywords.jsx
@@ -1,33 +1,30 @@
 import { Link } from "react-router-dom";
 import _ from "lodash";
 import { Box, Typography, Chip } from "@mui/material";
-import { lime } from "@mui/material/colors";
 
 const Keywords = ({ keywords }) => {
     return (
         <Box sx={{ mb: 4 }}>
             <Typography
                 variant="body1"
-                sx={{ letterSpacing: 2, fontWeight: "700", color: lime[500] }}
+                sx={{ letterSpacing: 2, fontWeight: "700" }}
             >
-                Keywords
+                Keywords:
             </Typography>
-            {_.isEmpty(keywords && keywords.results) ? (
-                <Typography variant="body2" sx={{ color: lime[100] }}>
+            {_.isEmpty(keywords?.results) ? (
+                <Typography variant="body2">
                     No keywords have been added.
                 </Typography>
             ) : (
                 <Box sx={{ display: "flex", gap: 1, flexWrap: "wrap", mt: 2 }}>
-                    {keywords.results.map((keyword) => (
+                    {keywords?.results.map((keyword) => (
                         <Link
                             key={keyword.id}
-                            to={`/keyword/${keyword.id}-${keyword.name &&
-                                keyword.name
-                                    .split(" ")
-                                    .join("-")
-                                    .split("--")
-                                    .join("-")
-                                    .toLowerCase()
+                            to={`/keyword/${keyword.id}-${keyword.name?.split(" ")
+                                .join("-")
+                                .split("--")
+                                .join("-")
+                                .toLowerCase()
                                 }/tv`}
                             style={{ textDecoration: "none" }}
                         >
--- src/components/pages/tvShows/TvShowFacts.jsx
+++ src/components/pages/tvShows/TvShowFacts.jsx
@@ -1,6 +1,6 @@
 import { Avatar, Box, Typography } from "@mui/material";
-import { lime } from "@mui/material/colors";
 import { Link } from "react-router-dom";
+
 import Keywords from "./Keywords";
 
 const TvShowFacts = ({
@@ -19,7 +19,7 @@ const TvShowFacts = ({
                 flexDirection: { xs: "row", md: "column" },
                 justifyContent: "space-between",
                 flexWrap: "wrap",
-                gap: 5,
+                gap: 3,
                 my: 4,
             }}
         >
@@ -28,11 +28,11 @@ const TvShowFacts = ({
                 <div>
                     <Typography
                         variant="body1"
-                        sx={{ letterSpacing: 2, fontWeight: "700", color: lime[500] }}
+                        sx={{ letterSpacing: 2, fontWeight: "700" }}
                     >
-                        Original Title
+                        Original Title:
                     </Typography>
-                    <Typography variant="body2" sx={{ color: lime[100] }}>
+                    <Typography variant="body2" sx={{ pl: 1, letterSpacing: 1 }}>
                         {original_name}
                     </Typography>
                 </div>
@@ -40,25 +40,25 @@ const TvShowFacts = ({
             <div>
                 <Typography
                     variant="body1"
-                    sx={{ letterSpacing: 2, fontWeight: "700", color: lime[500] }}
+                    sx={{ letterSpacing: 2, fontWeight: "700" }}
                 >
-                    Status
+                    Status:
                 </Typography>
-                <Typography variant="body2" sx={{ color: lime[100] }}>
+                <Typography variant="body2" sx={{ pl: 1, letterSpacing: 1 }}>
                     {status}
                 </Typography>
             </div>
             <div>
                 <Typography
                     variant="body1"
-                    sx={{ letterSpacing: 2, fontWeight: "700", color: lime[500] }}
+                    sx={{ letterSpacing: 2, fontWeight: "700" }}
                 >
-                    Network
+                    Network:
                 </Typography>
-                <Box sx={{display:'flex'}}>
+                <Box sx={{ display: 'flex',pl:1 }}>
                     {networks && networks.map((network) => (
                         <Link key={network.id} to={`/network/${network.id}`} style={{ textDecoration: 'none' }}>
-                            <Avatar variant="square" sx={{ maxWidth:260,width:1, maxHeight: 30 }} src={`https://www.themoviedb.org/t/p/h30${network.logo_path}`} alt={network.name} />
+                            <Avatar variant="square" sx={{ maxWidth: 260, width: 1, maxHeight: 30 }} src={`https://www.themoviedb.org/t/p/h30${network.logo_path}`} alt={network.name} />
                         </Link>
                     ))}
                 </Box>
@@ -66,25 +66,25 @@ const TvShowFacts = ({
             <div>
                 <Typography
                     variant="body1"
-                    sx={{ letterSpacing: 2, fontWeight: "700", color: lime[500] }}
+                    sx={{ letterSpacing: 2, fontWeight: "700" }}
                 >
-                    Type
+                    Type:
                 </Typography>
-                <Typography variant="body2" sx={{ color: lime[100] }}>
+                <Typography variant="body2" sx={{ pl: 1, letterSpacing: 1 }}>
                     {type}
                 </Typography>
             </div>
             <div>
                 <Typography
                     variant="body1"
-                    sx={{ letterSpacing: 2, fontWeight: "700", color: lime[500] }}
+                    sx={{ letterSpacing: 2, fontWeight: "700" }}
                 >
-                    Original Language
+                    Original Language:
                 </Typography>
                 {languagesList.map(
                     (language, index) =>
                         language.iso_639_1 === original_language && (
-                            <Typography key={index} variant="body2" sx={{ color: lime[100] }}>
+                            <Typography key={index} variant="body2" sx={{ pl: 1, letterSpacing: 1 }}>
                                 {language.english_name}
                             </Typography>
                         )
