5fe75cfbc0693ec8acc5cdc6f0186e10b784a75b
mojtaba <mojtaba75rs@gmail.com>
2023-03-10 15:55:24 +0330

    Crating index file for Media component

---
+++ src/components/pages/movie/media/index.js
@@ -0,0 +1,6 @@
+export { default as Videos } from "./Videos";
+export { default as Backdrops } from "./Backdrops";
+export { default as Posters } from "./Posters";
+export { default as MediaItemLength } from "./MediaItemLength";
+export { default as ViewAllMedia } from "./ViewAllMedia";
+export { default as NoMediaMessage } from "./NoMediaMessage";
