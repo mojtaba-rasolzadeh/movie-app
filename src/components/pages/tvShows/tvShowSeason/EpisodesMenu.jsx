8380192e05f000daa96de0d53a664dd2d537133d
mojtaba <mojtaba75rs@gmail.com>
2023-03-12 10:58:06 +0330

    Creating a menu component and selecting parts

---
+++ src/components/pages/tvShows/tvShowSeason/EpisodesMenu.jsx
@@ -0,0 +1,66 @@
+import { useState } from 'react';
+import { List, ListItem, ListItemIcon, ListItemText, Menu, MenuItem } from "@mui/material";
+import { ExpandLess, ExpandMore } from "@mui/icons-material";
+
+const EpisodesMenu = ({ episodes, selectedIndex, setSelectedIndex }) => {
+
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+
+    const handleClickListItem = (event) => {
+        setAnchorEl(event.currentTarget);
+    }
+
+    const handleMenuItemClick = (event, index) => {
+        setSelectedIndex(index);
+        setAnchorEl(null);
+    }
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    }
+
+    return (
+        <div>
+            <List component="nav" sx={{ bgcolor: 'background.paper' }}>
+                <ListItem
+                    button
+                    id="lock-button"
+                    aria-haspopup="listbox"
+                    aria-controls="lock-menu"
+                    aria-expanded={open ? 'true' : undefined}
+                    onClick={handleClickListItem}
+                    sx={{ display: 'flex', alignItems: 'center', gap: 1, borderRadius: 1 }}
+                >
+                    <ListItemText primary="Episodes" sx={{ letterSpacing: 1 }} />
+                    <ListItemIcon sx={{ minWidth: 0 }}>
+                        {open ? <ExpandLess fontSize='small' /> : <ExpandMore fontSize='small' />}
+                    </ListItemIcon>
+                </ListItem>
+            </List>
+            <Menu
+                id="lock-menu"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                MenuListProps={{
+                    'aria-labelledby': 'lock-button',
+                    role: 'listbox',
+                }}
+            >
+                {episodes.map((episode, index) => (
+                    <MenuItem
+                        key={episode}
+                        selected={index === selectedIndex}
+                        onClick={(event) => handleMenuItemClick(event, index)}
+                        sx={{ letterSpacing: 1 }}
+                    >
+                        episode {index + 1}
+                    </MenuItem>
+                ))}
+            </Menu>
+        </div>
+    );
+}
+
+export default EpisodesMenu;
