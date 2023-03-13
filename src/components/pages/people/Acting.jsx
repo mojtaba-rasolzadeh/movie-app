d61ce528d16310e093a5abb193d5936d141ea9db
mojtaba <mojtaba75rs@gmail.com>
2023-03-13 17:53:37 +0330

    Refactoring CombinedCredits & MovieCredits & TvCredits components ralated the Person page

--- src/components/pages/people/Acting.jsx
+++ src/components/pages/people/Acting.jsx
@@ -1,66 +1,27 @@
 import { useState } from "react";
-import {
-  Box,
-  Typography,
-  Menu,
-  MenuItem,
-  List,
-  ListItem,
-  ListItemText,
-} from "@mui/material";
-import { lime, yellow } from "@mui/material/colors";
-import { ArrowDropDown } from "@mui/icons-material";
+import { Box, Typography } from "@mui/material";
+import { yellow } from "@mui/material/colors";
 
-import CombinedCredits from "./CombinedCredits";
-import MovieCredits from "./MovieCredits";
-import TvCredits from "./TvCredits";
-
-// const options = ["all", "movies", "tv"];
+import { CombinedCredits, MovieCredits, TvCredits, ActingMediaMenu } from "./";
 
 const Acting = ({ combined_credits, movie_credits, tv_credits }) => {
-  const [anchorEl, setAnchorEl] = useState(null);
+console.log(combined_credits);
+  
   const [selectedIndex, setSelectedIndex] = useState(0);
-  const open = Boolean(anchorEl);
 
-  const options = [
-    // "all", "movies", "tv"
-    {
-      menuLabel: "all",
-      creditsLength: combined_credits && combined_credits.cast.length,
-    },
-    {
-      menuLabel: "movies",
-      creditsLength: movie_credits && movie_credits.cast.length,
-    },
-    {
-      menuLabel: "tv",
-      creditsLength: tv_credits && tv_credits.cast.length,
-    },
-  ];
-
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleMenuItemClick = (event, index) => {
-    setSelectedIndex(index);
-    setAnchorEl(null);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
   let content;
-  if (selectedIndex === 0) {
-    content = (
-      <CombinedCredits
-        combinedCredits={combined_credits && combined_credits.cast}
-      />
-    );
-  } else if (selectedIndex === 1) {
-    content = <MovieCredits movieCredits={movie_credits.cast} />;
-  } else if (selectedIndex === 2) {
-    content = <TvCredits tvCredits={tv_credits.cast} />;
+
+  switch (selectedIndex) {
+    case 0:
+      content = <CombinedCredits combinedCredits={combined_credits?.cast} />;
+      break;
+    case 1:
+      content = <MovieCredits movieCredits={movie_credits.cast} />;
+      break;
+    case 2:
+      content = <TvCredits tvCredits={tv_credits.cast} />;
+      break;
+    default:
   }
 
   return (
@@ -73,53 +34,16 @@ const Acting = ({ combined_credits, movie_credits, tv_credits }) => {
           my: 2,
         }}
       >
-        <Typography variant="h5" sx={{ color: yellow[200] }}>
-          Acting
-        </Typography>
-        <List component="nav" aria-label="select-item">
-          <ListItem
-            button
-            id="basic-button"
-            aria-controls={open ? "basic-menu" : undefined}
-            aria-haspopup="true"
-            aria-expanded={open ? "true" : undefined}
-            onClick={handleClick}
-            sx={{ textTransform: "capitalize", letterSpacing: 1 }}
-          >
-            {/* <ListItemText secondary={options[selectedIndex].menuLabel}> */}
-            <ListItemText sx={{color:lime[500]}}>{options[selectedIndex].menuLabel}</ListItemText>
-            <ArrowDropDown />
-          </ListItem>
-        </List>
+        <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', gap: 3 }}>
+          <Typography variant="h5" sx={{ color: yellow[700] }}>
+            Acting
+          </Typography>
+        </Box>
+        <ActingMediaMenu
+          selectedIndex={selectedIndex}
+          setSelectedIndex={setSelectedIndex}
+        />
       </Box>
-      <Menu
-        id="basic-menu"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        MenuListProps={{
-          "aria-labelledby": "basic-button",
-        }}
-        variant="selectedMenu"
-      >
-        {options.map((option, index) => (
-          <MenuItem
-            key={index}
-            selected={index === selectedIndex}
-            onClick={(event) => handleMenuItemClick(event, index)}
-            sx={{
-              display: "flex",
-              alignItems: "center",
-              gap: 2,
-            }}
-          >
-            {option.menuLabel}
-            <Typography variant="caption" color="text.secondary" sx={{mt:'4px'}}>
-              {option.creditsLength}
-            </Typography>
-          </MenuItem>
-        ))}
-      </Menu>
       {content}
     </>
   );
--- src/components/pages/people/MovieCredits.jsx
+++ src/components/pages/people/MovieCredits.jsx
@@ -1,83 +1,57 @@
 import { Link } from 'react-router-dom';
-import _ from "lodash";
-import {
-  IconButton,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableRow,
-  Typography,
-} from "@mui/material";
-import { PlagiarismRounded } from "@mui/icons-material";
-import { deepPurple, teal, lightGreen } from "@mui/material/colors";
+import _ from 'lodash';
+import { Avatar, Box, CardActionArea, Chip, Divider, Typography, } from "@mui/material";
+import { grey, yellow } from "@mui/material/colors";
+import { GradeRounded } from '@mui/icons-material';
 
 const MovieCredits = ({ movieCredits }) => {
   return (
-    <TableContainer>
-      <Table>
-        <TableBody>
-          {movieCredits
-            .sort((a, b) => new Date(b.release_date) - new Date(a.release_date))
-            .map((movie) => (
-              <TableRow key={movie.id}>
-                <TableCell
-                  align="left"
-                  sx={{
-                    width: 24,
-                    padding: "8px 16px",
-                    color: lightGreen[500],
-                    textAlign: "center",
-                  }}
-                >
-                  {_.isEmpty(movie.release_date)
-                    ? "—"
-                    : movie.release_date.slice(0, 4)}
-                </TableCell>
-                <TableCell align="left" padding="none" sx={{ width: 24 }}>
-                  <IconButton>
-                    <PlagiarismRounded sx={{ color: deepPurple[500] }} />
-                  </IconButton>
-                </TableCell>
-                <TableCell align="left">
-                  <Link
-                    to={`/movie/${movie.id}`}
-                    style={{ textDecoration: 'none' }}
-
-                  >
-                    <Typography component="span" variant="body1" sx={{
-                      fontSize: "1rem",
-                      fontWeight: 700,
-                      color: teal[500],
-                      "&:hover": { color: teal[700] },
-                    }}>
-
-                      {movie.title}
-                    </Typography>
-                  </Link>
-                  {!_.isEmpty(movie.character) && (
-                    <>
-                      <Typography
-                        component="span"
-                        variant="subtitle2"
-                        sx={{ mx: 1, color: teal[50] }}
-                      >
-                        as
-                      </Typography>
-                      <Typography
-                        component="span"
-                        sx={{ fontWeight: 200, color: teal[100] }}
-                      >
-                        {movie.character}
-                      </Typography>
-                    </>
-                  )}
-                </TableCell>
-              </TableRow>
-            ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <>
+      <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between', alignItems: 'center', gap: 2, mb: 2 }}>
+        <Typography variant="h6" sx={{ letterSpacing: 1 }}>Movies</Typography>
+        <Chip label={movieCredits?.length} size="small" sx={{ background: 'linear-gradient(to right,#ED4700,#E76F00)' }} />
+      </Box>
+      <Divider />
+      <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', gap: 2, mt: 3 }}>
+        {movieCredits?.sort(
+          (a, b) => new Date(b.release_date) - new Date(a.release_date))
+          .map((movie) => (
+            <CardActionArea key={movie.id} sx={{ width: 160, height: 240, borderRadius: '20px' }}>
+              <Link
+                to={`/movie/${movie.id}`}
+                style={{ textDecoration: 'none' }}>
+                <Box Box key={movie.id} sx={{ position: 'relative', width: 160, borderRadius: '20px' }}>
+                  <Avatar variant="rounded" sx={{ width: 1, height: 240, borderRadius: '20px' }} src={`https://www.themoviedb.org/t/p/w220_and_h330_face${movie.poster_path}`} />
+                  <Box sx={{ position: 'absolute', top: 0, right: 0, bottom: 0, left: 0, width: 1, backgroundImage: 'linear-gradient(to top, rgb(32 32 32 / 94%) 90px, rgb(12 11 2 / 0%) 100%)', borderRadius: '17px' }} />
+                  <Box sx={{ width: 1, position: 'absolute', bottom: 10, p: 1, display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', textAlign: 'center' }}>
+                    <Box sx={{ maxWidth: 200 }}>
+                      <Box sx={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center', gap: 1, mb: 1 }}>
+                        <Chip size='small' sx={{ backgroundColor: grey[900] }} icon={<GradeRounded fontSize='small' sx={{ color: `${yellow['A700']} !important` }} />} label={
+                          <Typography variant='caption'>{movie.vote_average?.toFixed(1)}</Typography>
+                        } />
+                        <Chip size='small' sx={{ backgroundColor: grey[900] }} label={
+                          <Typography variant='caption'>
+                            {_.isEmpty(movie.release_date)
+                              ? "—"
+                              : movie.release_date.slice(0, 4)}
+                          </Typography>
+                        } />
+                      </Box>
+                      <Link
+                        to={`/movie/${movie.id}`}
+                        style={{ textDecoration: 'none' }}>
+                        <Typography variant="body2" sx={{ textAlign: 'center', letterSpacing: 1, color: '#fff', '&:hover': { color: 'text.secondary' } }}>
+                          {movie.title}
+                        </Typography>
+                      </Link>
+                    </Box>
+                  </Box>
+                </Box>
+              </Link>
+            </CardActionArea>
+          ))}
+      </Box>
+    </>
   );
 };
 
