01a36db5cf9be3e2578637dd557f716b3f0e8bc1
mojtaba <mojtaba75rs@gmail.com>
2023-03-13 17:45:32 +0330

    Refactoring and change color text a PersonalInfo component ralated the Person page

--- src/components/pages/people/PersonalInfo.jsx
+++ src/components/pages/people/PersonalInfo.jsx
@@ -1,6 +1,6 @@
 import _ from "lodash";
 import { Box, Typography, Divider } from "@mui/material";
-import { amber, lime } from "@mui/material/colors";
+import { yellow } from "@mui/material/colors";
 
 const PersonalInfo = ({
   known_for_department,
@@ -10,6 +10,7 @@ const PersonalInfo = ({
   also_known_as,
   combined_credits,
 }) => {
+
   const calculateAge = (birthday) => {
     let ageDifMs = Date.now() - new Date(birthday).getTime();
     let ageDate = new Date(ageDifMs);
@@ -19,70 +20,76 @@ const PersonalInfo = ({
   return (
     <Box>
       <Divider>
-        <Typography variant="h5" sx={{ fontWeight: 700, color: amber[500] }}>
+        <Typography variant="h5" sx={{
+          fontWeight: 700,
+          background: 'linear-gradient(to right,#ED4700,#E76F00)',
+          WebkitBackgroundClip: 'text',
+          WebkitTextFillColor: 'transparent',
+          letterSpacing: 1,
+        }}>
           Personal Info
         </Typography>
       </Divider>
       <Box sx={{ display: "flex", flexDirection: "column", gap: 3, mt: 4 }}>
         <Box>
-          <Typography sx={{ fontWeight: 800, color: lime[500] }}>
-            Known For
+          <Typography variant="body1" sx={{ fontWeight: 700, letterSpacing: 1, color: yellow[700] }}>
+            Known For:
           </Typography>
-          <Typography variant="body2" sx={{ color: lime[100] }}>
+          <Typography variant="body2" sx={{ pl: 1 }} >
             {_.isEmpty(known_for_department) ? "-" : known_for_department}
           </Typography>
         </Box>
         <Box>
-          <Typography sx={{ fontWeight: 800, color: lime[500] }}>
-            Known Credits
+          <Typography variant="body1" sx={{ fontWeight: 700, letterSpacing: 1, color: yellow[700] }}>
+            Known Credits:
           </Typography>
-          <Typography variant="body2" sx={{ color: lime[100] }}>
+          <Typography variant="body2" sx={{ pl: 1 }}>
             {_.isEmpty(combined_credits)
               ? "-"
-              : combined_credits && combined_credits.cast.length}
+              : combined_credits?.cast.length}
           </Typography>
         </Box>
         <Box>
-          <Typography sx={{ fontWeight: 800, color: lime[500] }}>
-            Gender
+          <Typography variant="body1" sx={{ fontWeight: 700, letterSpacing: 1, color: yellow[700] }}>
+            Gender:
           </Typography>
-          <Typography variant="body2" sx={{ color: lime[100] }}>
+          <Typography variant="body2" sx={{ pl: 1 }}>
             {gender === 0 ? "-" : gender === 1 ? "Female" : "Male"}
           </Typography>
         </Box>
         <Box>
-          <Typography sx={{ fontWeight: 800, color: lime[500] }}>
-            Birthday
+          <Typography variant="body1" sx={{ fontWeight: 700, letterSpacing: 1, color: yellow[700] }}>
+            Birthday:
           </Typography>
-          <Typography variant="body2" sx={{ color: lime[100] }}>
+          <Typography variant="body2" sx={{ pl: 1 }}>
             {_.isEmpty(birthday)
               ? "-"
               : `${birthday} ( ${calculateAge(birthday)} years old )`}
           </Typography>
         </Box>
         <Box>
-          <Typography sx={{ fontWeight: 800, color: lime[500] }}>
-            Place of Birth
+          <Typography variant="body1" sx={{ fontWeight: 700, letterSpacing: 1, color: yellow[700] }}>
+            Place of Birth:
           </Typography>
-          <Typography variant="body2" sx={{ color: lime[100] }}>
+          <Typography variant="body2" sx={{ pl: 1 }}>
             {_.isEmpty(place_of_birth) ? "-" : place_of_birth}
           </Typography>
         </Box>
         <Box>
-          <Typography sx={{ fontWeight: 800, color: lime[500] }}>
-            Also Known As
+          <Typography variant="body1" sx={{ fontWeight: 700, letterSpacing: 1, color: yellow[700] }}>
+            Also Known As:
           </Typography>
           {_.isEmpty(also_known_as)
             ? "-"
             : also_known_as.map((item, index) => (
-                <Typography
-                  key={index}
-                  variant="body2"
-                  sx={{ color: lime[100], my: 1 }}
-                >
-                  {item}
-                </Typography>
-              ))}
+              <Typography
+                key={index}
+                variant="body2"
+                sx={{ my: 1, pl: 1 }}
+              >
+                {item}
+              </Typography>
+            ))}
         </Box>
       </Box>
       <Divider sx={{ display: { xs: "block", sm: "none" }, my: 4 }} />
