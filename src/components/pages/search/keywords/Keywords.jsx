97d419204b9db99c2ec1521959f912056233f51e
mojtaba <mojtaba75rs@gmail.com>
2023-03-14 15:03:37 +0330

    Refactoring the Keywords component related to the Search page

--- src/components/pages/search/keywords/Keywords.jsx
+++ src/components/pages/search/keywords/Keywords.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import _ from "lodash";
+import { Link } from "react-router-dom";
 import {
   Box,
   Divider,
@@ -8,23 +9,20 @@ import {
   ListItemButton,
   ListItemText,
   Typography,
-  Stack,
-  Pagination,
 } from "@mui/material";
-import { Link } from "react-router-dom";
+
 import { getSearchKeywords } from "../../../../services/MovieService";
 import { Loader } from "../../../constant";
+import MoviePagination from "../../movie/MoviePagination";
 
 const Keywords = ({ keywordsData, query }) => {
   const [loading, setLoading] = useState(false);
   const [keywords, setKeywords] = useState({ ...keywordsData });
-  const [page, setPage] = useState(1);
 
-  const handleChangePage = async (event, value) => {
-    setPage(value);
+  const handleChangePage = async (page) => {
     try {
       setLoading(true);
-      const { status, data } = await getSearchKeywords(query, value);
+      const { status, data } = await getSearchKeywords(query, page);
       if (status === 200) {
         setLoading(false);
         setKeywords(data);
@@ -42,7 +40,7 @@ const Keywords = ({ keywordsData, query }) => {
           There are no keywords that matched your query.
         </Typography>
       ) : (
-        <Stack>
+        <>
           {loading ? (
             <Loader />
           ) : (
@@ -70,17 +68,8 @@ const Keywords = ({ keywordsData, query }) => {
               ))}
             </List>
           )}
-          {keywords.total_pages > 1 && (
-            <Pagination
-              count={keywords.total_pages}
-              page={page}
-              onChange={handleChangePage}
-              variant="outlined"
-              shape="rounded"
-              sx={{ ".MuiPagination-ul": { justifyContent: "center" } }}
-            />
-          )}
-        </Stack>
+          <MoviePagination movieData={keywords} fetchData={handleChangePage} />
+        </>
       )}
     </>
   );
