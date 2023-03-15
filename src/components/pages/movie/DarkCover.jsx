de7d7ac6b95687728a8110bc2584fbc9ba7fc550
mojtaba <mojtaba75rs@gmail.com>
2023-03-15 19:50:09 +0330

    Edit the DarkCover component for the Movie page

--- src/components/pages/movie/DarkCover.jsx
+++ src/components/pages/movie/DarkCover.jsx
@@ -9,10 +9,12 @@ const DarkCover = () => {
                 right: 0,
                 bottom: 0,
                 left: 0,
+                borderBottomLeftRadius:'20px',
+                borderBottomRightRadius:'20px',
                 backgroundImage: {
-                    xs: "linear-gradient(to right, rgba(31.5, 31.5, 31.5, 0.84) 150px, rgba(31.5, 31.5, 31.5, 0.84) 100%)",
-                    md: "linear-gradient(to right, rgba(31.5, 31.5, 31.5, 1) 150px, rgba(31.5, 31.5, 31.5, 0.84) 100%)",
-                },
+                    xs: "linear-gradient(to right, rgba(32, 32, 32 , 73%) 150px, rgba(32, 32, 32 , 73%) 100%)",
+                    // md: "linear-gradient(to right, rgba(31.5, 31.5, 31.5, 1) 150px, rgba(31.5, 31.5, 31.5, 0.84) 100%)",
+                }
             }}
         />
     );
