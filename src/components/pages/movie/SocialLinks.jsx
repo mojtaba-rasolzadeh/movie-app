e39d74cbd4a67ba461e4818e57a54cd33b694f10
mojtaba <mojtaba75rs@gmail.com>
2023-03-18 18:13:36 +0330

    Editing the SocialLinks component related to the Movie page

--- src/components/pages/movie/SocialLinks.jsx
+++ src/components/pages/movie/SocialLinks.jsx
@@ -1,62 +1,55 @@
-import { Divider, Box, IconButton, Link, Tooltip } from "@mui/material";
+import { IconButton, Tooltip, Card } from "@mui/material";
 import {
-  Facebook,
-  InsertLinkRounded,
+  FacebookRounded,
   Instagram,
   LinkRounded,
   SlowMotionVideo,
   Twitter,
 } from "@mui/icons-material";
+import { indigo } from "@mui/material/colors";
 
-const SocialLinks = ({ facebook_id, twitter_id, instagram_id, homepage }) => {
+const SocialLinks = ({ external_ids, homepage }) => {
+
   return (
-    <Box sx={{ display: "flex", gap: 0.5, flexWrap: "wrap" }}>
-      {facebook_id && (
-        <Tooltip title="Visit Facebook" placement="top">
-          <IconButton
-            color="info"
-            href={`https://facebook.com/${facebook_id}`}
-            target="_blank"
-          >
-            <Facebook />
-          </IconButton>
-        </Tooltip>
-      )}
-      {twitter_id && (
-        <Tooltip title="Visit Twitter" placement="top">
-          <IconButton
-            color="info"
-            href={`https://twitter.com/${twitter_id}`}
-            target="_blank"
-          >
-            <Twitter />
-          </IconButton>
-        </Tooltip>
-      )}
-      {instagram_id && (
-        <Tooltip title="Visit Instagram" placement="top">
-          <IconButton
-            color="error"
-            href={`https://instagram.com/${instagram_id}/`}
-            target="_blank"
-          >
-            <Instagram />
-          </IconButton>
-        </Tooltip>
-      )}
-      {/* <Tooltip title="Visit JustWatch" placement="top">
-        <IconButton color="success">
+    <Card sx={{ display: "flex", flexWrap: "wrap", justifyContent: 'center', alignItems: 'center', gap: 0.5, mt: { xs: 4, md: 1 }, p: 2, borderRadius: '20px' }}>
+      <Tooltip title="Visit Facebook" placement="top">
+        <IconButton
+          href={`https://facebook.com/${external_ids?.facebook_id}`}
+          target="_blank"
+          sx={{ color: indigo[800] }}
+        >
+          <FacebookRounded />
+        </IconButton>
+      </Tooltip>
+      <Tooltip title="Visit Twitter" placement="top">
+        <IconButton
+          color="info"
+          href={`https://twitter.com/${external_ids?.twitter_id}`}
+          target="_blank"
+        >
+          <Twitter />
+        </IconButton>
+      </Tooltip>
+      <Tooltip title="Visit Instagram" placement="top">
+        <IconButton
+          color="error"
+          href={`https://instagram.com/${external_ids?.instagram_id}/`}
+          target="_blank"
+        >
+          <Instagram />
+        </IconButton>
+      </Tooltip>
+      <Tooltip title="Visit IMDB" placement="top">
+        <IconButton color="success" href={`https://m.imdb.com/title/${external_ids?.imdb_id}`} target="_blank">
           <SlowMotionVideo />
         </IconButton>
-      </Tooltip> */}
-      {homepage && (
-        <Tooltip title="Visit Homepage" placement="top">
-          <IconButton color="warning" href={homepage} target="_blank">
-            <LinkRounded />
-          </IconButton>
-        </Tooltip>
-      )}
-    </Box>
+      </Tooltip>
+      <Tooltip title="Visit Homepage" placement="top">
+        <IconButton color="warning" href={homepage} target="_blank">
+          <LinkRounded />
+        </IconButton>
+      </Tooltip>
+    </Card>
   );
 };
 
