28b9e0408d56743a83ec3c6b976177e7ca125c12
mojtaba <mojtaba75rs@gmail.com>
2023-03-27 02:41:53 +0430

    Remove unused components

--- src/components/discover/filter/index.js
+++
@@ -1,8 +0,0 @@
-export { default as Availabilities } from "./Availabilities";
-export { default as ReleaseDates } from "./ReleaseDates";
-export { default as Genres } from "./Genres";
-export { default as Certification } from "./Certification";
-export { default as Language } from "./Language";
-export { default as UserScore } from "./UserScore";
-export { default as UserVotes } from "./UserVotes";
-export { default as Runtime } from "./Runtime";
--- src/components/discover/watch/index.js
+++
@@ -1,2 +0,0 @@
-export { default as AutocompleteCountry } from "./AutocompleteCountry";
-export { default as ProviderMovies } from "./ProviderMovies";
