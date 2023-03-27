1859e61778a8e681d827b8ec9e1e6a3c1d3b65f6
mojtaba <mojtaba75rs@gmail.com>
2023-03-28 01:29:31 +0430

    Hide the Hamburger Menu Button when showing the sidebar

--- src/components/layouts/navbar/HamburgerMenuButton.jsx
+++ src/components/layouts/navbar/HamburgerMenuButton.jsx
@@ -1,19 +1,35 @@
+import { MenuRounded } from "@mui/icons-material";
 import { IconButton } from "@mui/material";
 
 const HamburegerMenuButton = ({ mobileOpen, handleDrawerToggle }) => {
   return (
-    <IconButton
-      sx={{ display: { sm: "none" } }}
-      size="large"
-      aria-label="menu"
-      className={`hamburger ${mobileOpen ? "open" : null} md:hidden`}
-      onClick={handleDrawerToggle}
-    >
-      <span className="hamburger-top"></span>
-      <span className="hamburger-middle"></span>
-      <span className="hamburger-bottom"></span>
-    </IconButton>
+    <>
+      {!mobileOpen && (
+        <IconButton
+          sx={{ display: { sm: "none" } }}
+          size="large"
+          aria-label="menu"
+          onClick={handleDrawerToggle}
+        >
+          <MenuRounded />
+        </IconButton>
+      )}
+    </>
   );
 };
 
 export default HamburegerMenuButton;
+
+// {!mobileOpen && (
+//   <IconButton
+//     sx={{ display: { sm: "none" } }}
+//     size="large"
+//     aria-label="menu"
+//     className={`hamburger ${mobileOpen ? "open" : null} md:hidden`}
+//     onClick={handleDrawerToggle}
+//   >
+//     <span className="hamburger-top"></span>
+//     <span className="hamburger-middle"></span>
+//     <span className="hamburger-bottom"></span>
+//   </IconButton>
+// )}
